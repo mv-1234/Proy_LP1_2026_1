980715e14843204c35bde8018840e8da8f9d4841
Mauricio <mgonzalovela@gmail.com>
2026-06-22 18:02:52 -0500

    Comentarios

--- huffman_jpeg.h
+++ huffman_jpeg.h
@@ -7,17 +7,17 @@
 
 // Estructura para representar un nodo en el arbol de Huffman
 typedef struct NodoHuffman {
-    int valor;                 // El valor a comprimir
-    int frecuencia;            // Cantidad de veces que aparece este valor (peso del nodo)
+    int valor; // El valor a comprimir
+    int frecuencia; // Cantidad de veces que aparece este valor (peso del nodo)
     struct NodoHuffman *izq, *der; // Punteros a los hijos izquierdo y derecho
 } NodoHuffman;
 
 // Estructura para escribir datos bit a bit en un archivo,
 // ya que las funciones estandar de C escriben byte a byte
 typedef struct {
-    FILE *archivo;             // Puntero al archivo de salida
-    unsigned char buffer;      // Acumulador de bits (hasta llegar a 8)
-    int bits_en_buffer;        // Contador de cuantos bits hay actualmente en el buffer
+    FILE *archivo; // Puntero al archivo de salida
+    unsigned char buffer; // Acumulador de bits (hasta llegar a 8)
+    int bits_en_buffer; // Contador de cuantos bits hay actualmente en el buffer
 } BitWriter;
 
 // Declaraciones de funciones
